ae9c4deb5847edf5d05f1b32ef8478f7685404ea
Chris Perrins <christoper.perrins@qa.com>
2019-02-01 09:11:38 +0000

    -y added to install

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt install openjdk-8-jdk
-sudo apt install maven
+sudo apt install -y openjdk-8-jdk
+sudo apt install -y maven
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 #sudo apt update
-sudo apt install jenkins
+sudo apt install -y jenkins
 
 
 
