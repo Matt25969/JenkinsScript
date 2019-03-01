2d712f79d0e486444d85173b43a5c023c272a3c7
Chris Perrins <40370810+christophperrins@users.noreply.github.com>
2019-03-01 08:51:26 +0000

    Removed phantomScript

--- jenkins.sh
+++ jenkins.sh
@@ -15,27 +15,5 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-PHANTOM_VERSION="phantomjs-1.9.8"
-ARCH=$(uname -m)
-
-if ! [ $ARCH = "x86_64" ]; then
-	$ARCH="i686"
-fi
-
-PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
-
-sudo apt-get update
-sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
-sudo apt-get install libfreetype6 libfreetype6-dev -y
-sudo apt-get install libfontconfig1 libfontconfig1-dev -y
-
-cd ~
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-sudo tar xvjf $PHANTOM_JS.tar.bz2
-
-sudo mv $PHANTOM_JS /usr/local/share
-sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
-
-
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
